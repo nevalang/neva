a363f259982fc6cdec7d41fb0cddd4cb6904a147
Emil Valeev <emil.musician@gmail.com>
2023-02-18 12:39:04 +0600

    refactor(ts): get rid of separate stringer.go

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -1,5 +1,7 @@
 package types
 
+import "fmt"
+
 type Def struct { // TODO add validation
 	Params             []Param // Body can refer to these parameters
 	BodyExpr           Expr    // Empty body here means base type (TODO maybe change needed)
@@ -41,6 +43,70 @@ type Expr struct {
 // Empty returns true if inst and lit both empty
 func (expr Expr) Empty() bool {
 	return expr.Inst.Empty() && expr.Lit.Empty()
+}
+
+// String formats expression in a TS manner
+func (expr Expr) String() string { // todo move?
+	if expr.Empty() {
+		return "empty"
+	}
+
+	var str string
+
+	switch expr.Lit.Type() {
+	case ArrLitType:
+		return fmt.Sprintf(
+			"[%d]%s",
+			expr.Lit.Arr.Size, expr.Lit.Arr.Expr.String(),
+		)
+	case EnumLitType:
+		str += "{"
+		for i, el := range expr.Lit.Enum {
+			str += " " + el
+			if i == len(expr.Lit.Enum)-1 {
+				str += " "
+			} else {
+				str += ","
+			}
+		}
+		return str + "}"
+	case RecLitType:
+		str += "{"
+		count := 0
+		for fieldName, fieldExpr := range expr.Lit.Rec {
+			str += fmt.Sprintf(" %s %s", fieldName, fieldExpr)
+			if count < len(expr.Lit.Rec)-1 {
+				str += ","
+			} else {
+				str += " "
+			}
+			count++
+		}
+		return str + "}"
+	case UnionLitType:
+		for i, el := range expr.Lit.Union {
+			str += el.String()
+			if i < len(expr.Lit.Union)-1 {
+				str += " | "
+			}
+		}
+		return str
+	}
+
+	if len(expr.Inst.Args) == 0 {
+		return expr.Inst.Ref
+	}
+
+	str = expr.Inst.Ref + "<"
+	for i, arg := range expr.Inst.Args {
+		str += arg.String()
+		if i < len(expr.Inst.Args)-1 {
+			str += ", "
+		}
+	}
+	str += ">"
+
+	return str
 }
 
 // Instantiation expression
--- pkg/types/types_test.go
+++ pkg/types/types_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	ts "github.com/emil14/neva/pkg/types"
+	types "github.com/emil14/neva/pkg/types"
 	"github.com/stretchr/testify/require"
 )
 
@@ -168,6 +169,225 @@ func TestDef_String(t *testing.T) {
 			if got := tt.def.String(); got != tt.want {
 				t.Errorf("Def.String() = %v, want %v", got, tt.want)
 			}
+		})
+	}
+}
+
+func TestExpr_String(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name string
+		expr types.Expr
+		want string
+	}{
+		// insts
+		{
+			name: "empty_expr_(inst_with_empty_ref_and_no_args)",
+			expr: types.Expr{},
+			want: "empty",
+		},
+		{
+			name: "inst expr with non-empty ref and no args",
+			expr: types.Expr{
+				Inst: types.InstExpr{Ref: "int"},
+			},
+			want: "int",
+		},
+		{
+			name: "inst expr with empty refs and with args",
+			expr: types.Expr{
+				Inst: types.InstExpr{
+					Args: []types.Expr{
+						{Inst: types.InstExpr{Ref: "str"}},
+					},
+				},
+			},
+			want: "<str>",
+		},
+		{
+			name: "inst expr with non-empty refs and with args",
+			expr: types.Expr{
+				Inst: types.InstExpr{
+					Ref: "map",
+					Args: []types.Expr{
+						{Inst: types.InstExpr{Ref: "str"}},
+					},
+				},
+			},
+			want: "map<str>",
+		},
+		{
+			name: "inst expr with non-empty refs and with several args",
+			expr: types.Expr{
+				Inst: types.InstExpr{
+					Ref: "map",
+					Args: []types.Expr{
+						{Inst: types.InstExpr{Ref: "str"}},
+						{Inst: types.InstExpr{Ref: "bool"}},
+					},
+				},
+			},
+			want: "map<str, bool>",
+		},
+		{
+			name: "inst expr with non-empty refs and with nested arg",
+			expr: types.Expr{
+				Inst: types.InstExpr{
+					Ref: "map",
+					Args: []types.Expr{
+						{Inst: types.InstExpr{Ref: "str"}},
+						{
+							Inst: types.InstExpr{
+								Ref: "vec",
+								Args: []types.Expr{
+									{Inst: types.InstExpr{Ref: "bool"}},
+								},
+							},
+						},
+					},
+				},
+			},
+			want: "map<str, vec<bool>>",
+		},
+		// Lits
+		// enum
+		{
+			name: "lit_expr_empty_enum",
+			expr: types.Expr{
+				Lit: types.LitExpr{
+					Enum: []string{},
+				},
+			},
+			want: "{}",
+		},
+		{
+			name: "lit expr enum with one el",
+			expr: types.Expr{
+				Lit: types.LitExpr{
+					Enum: []string{"MONDAY"},
+				},
+			},
+			want: "{ MONDAY }",
+		},
+		{
+			name: "lit expr enum with two els",
+			expr: types.Expr{
+				Lit: types.LitExpr{
+					Enum: []string{"MONDAY", "TUESDAY"},
+				},
+			},
+			want: "{ MONDAY, TUESDAY }",
+		},
+		// arr
+		{
+			name: "lit_expr_arr_with_size_0_and_without type",
+			expr: types.Expr{
+				Lit: types.LitExpr{
+					Arr: &types.ArrLit{},
+				},
+			},
+			want: "[0]empty",
+		},
+		{
+			name: "lit expr arr with size 0 and with type",
+			expr: types.Expr{
+				Lit: types.LitExpr{
+					Arr: &types.ArrLit{
+						Expr: types.Expr{Inst: types.InstExpr{Ref: "int"}},
+					},
+				},
+			},
+			want: "[0]int",
+		},
+		{
+			name: "lit expr arr with size 4096 and with type",
+			expr: types.Expr{
+				Lit: types.LitExpr{
+					Arr: &types.ArrLit{
+						Size: 4096,
+						Expr: types.Expr{Inst: types.InstExpr{Ref: "int"}},
+					},
+				},
+			},
+			want: "[4096]int",
+		},
+		// rec
+		{
+			name: "lit expr rec no fields",
+			expr: types.Expr{
+				Lit: types.LitExpr{
+					Rec: map[string]types.Expr{},
+				},
+			},
+			want: "{}",
+		},
+		{
+			name: "lit expr rec with one field",
+			expr: types.Expr{
+				Lit: types.LitExpr{
+					Rec: map[string]types.Expr{
+						"name": {Inst: types.InstExpr{Ref: "str"}},
+					},
+				},
+			},
+			want: "{ name str }",
+		},
+		{ // FIXME flacky test (struct must be ordered)
+			name: "lit expr rec with two fields",
+			expr: types.Expr{
+				Lit: types.LitExpr{
+					Rec: map[string]types.Expr{
+						"name": {Inst: types.InstExpr{Ref: "str"}},
+						"age":  {Inst: types.InstExpr{Ref: "int"}},
+					},
+				},
+			},
+			want: "{ name str, age int }",
+		},
+		// union
+		{
+			name: "lit expr empty union", // not a valid expr
+			expr: types.Expr{
+				Lit: types.LitExpr{
+					Union: []types.Expr{},
+				},
+			},
+			want: "",
+		},
+		{
+			name: "lit expr union with one el", // not a valid expr
+			expr: types.Expr{
+				Lit: types.LitExpr{
+					Union: []types.Expr{
+						{Inst: types.InstExpr{Ref: "int"}},
+					},
+				},
+			},
+			want: "int",
+		},
+		{
+			name: "lit expr union with two els",
+			expr: types.Expr{
+				Lit: types.LitExpr{
+					Union: []types.Expr{
+						{Inst: types.InstExpr{Ref: "int"}},
+						{Inst: types.InstExpr{Ref: "str"}},
+					},
+				},
+			},
+			want: "int | str",
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			require.Equal(
+				t, tt.want, tt.expr.String(),
+			)
 		})
 	}
 }
