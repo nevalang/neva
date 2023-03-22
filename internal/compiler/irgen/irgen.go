0fbc393e82327483faa2540540819bd43b85d4ab
Emil Valeev <efvaleev@avito.ru>
2023-03-22 22:12:57 +0300

    wip

--- internal/compiler/irgen/irgen.go
+++ internal/compiler/irgen/irgen.go
@@ -23,48 +23,58 @@ func (g Generator) Generate(ctx context.Context, prog compiler.Program) (ir.Prog
 		panic("")
 	}
 
-	ref := compiler.EntityRef{"main", "main"}
+	ref := compiler.EntityRef{Pkg: "main", Name: "main"}
 	rootNodeCtx := nodeContext{
 		componentRef: ref,
 		io: ioContext{
-			in: map[string][]portContext{
+			in: map[string][]slotContext{
 				"start": {
 					{incoming: 1},
 				},
 			},
-			out: map[string][]portContext{
-				"start": {
+			out: map[string][]slotContext{
+				"exit": {
+					// FIXME buf size could not be computed from the outside of the node's network
+					// we need to see how many incoming connections outport has inside node as an inport
+					// maybe we need here information about the outgoing connections
 					{incoming: 1},
 				},
 			},
 		},
 	}
 
-	if err := g.generate(ctx, rootNodeCtx, prog.Pkgs); err != nil {
+	result := ir.Program{
+		Ports:       map[ir.PortAddr]uint8{},
+		Routines:    ir.Routines{},
+		Connections: []ir.Connection{},
+	}
+	if err := g.processNode(ctx, rootNodeCtx, prog.Pkgs, &result); err != nil {
 		panic(err)
 	}
 
-	return ir.Program{}, nil
+	return result, nil
 }
 
 type (
 	nodeContext struct {
+		path         string
 		componentRef compiler.EntityRef
 		io           ioContext
 	}
 	ioContext struct {
-		in, out map[string][]portContext
+		in, out map[string][]slotContext
 	}
-	portContext struct {
+	slotContext struct {
 		incoming  uint8              // count of incoming connections (buffer should be -1)
 		staticMsg *compiler.MsgValue // only for static inports
 	}
 )
 
-func (g Generator) generate(
+func (g Generator) processNode(
 	ctx context.Context,
 	nodeCtx nodeContext,
 	pkgs map[string]compiler.Pkg,
+	result *ir.Program,
 ) error {
 	mainPkg, ok := pkgs[nodeCtx.componentRef.Pkg]
 	if !ok {
@@ -76,20 +86,64 @@ func (g Generator) generate(
 		panic("")
 	}
 
-	irPorts := make(
-		map[ir.PortAddr]uint8,
-		len(mainEntity.Component.IO.In)+len(mainEntity.Component.IO.Out),
-	)
+	component := mainEntity.Component
 
-	for name, port := range mainEntity.Component.IO.In {
-		portCtxs, ok := nodeCtx.io.in[name]
+	// create IR ports for this node
+
+	// irPorts := make(
+	// 	map[ir.PortAddr]uint8,
+	// 	len(component.IO.In),
+	// )
+	for name := range component.IO.In {
+		slotCtxs, ok := nodeCtx.io.in[name]
 		if !ok {
 			panic("")
 		}
-
-		for _, portCtx := range portCtxs {
-			portCtx.incoming
+		for _, slotCtx := range slotCtxs {
+			addr := ir.PortAddr{Path: nodeCtx.path, Name: name}
+			result.Ports[addr] = slotCtx.incoming
 		}
-
 	}
+
+	// outPorts := make(
+	// 	map[ir.PortAddr]uint8,
+	// 	len(component.IO.In),
+	// )
+	for name := range component.IO.Out {
+		slotCtxs, ok := nodeCtx.io.out[name]
+		if !ok {
+			panic("")
+		}
+		for _, slotCtx := range slotCtxs {
+			addr := ir.PortAddr{Path: nodeCtx.path, Name: name}
+			result.Ports[addr] = slotCtx.incoming
+		}
+	}
+
+	// create connections for this node and
+	// irConnections := make([]ir.Connection, 0, len(component.Net))
+
+	for _, conn := range component.Net {
+		senderAddr := ir.PortAddr{
+			Path: nodeCtx.path + "/" + conn.SenderSide.PortAddr.Node,
+			Name: conn.SenderSide.PortAddr.Name,
+			Idx:  conn.SenderSide.PortAddr.Idx,
+		}
+		senderSelectors := make([]ir.Selector, 0, len(conn.SenderSide.Selectors))
+		for _, selector := range conn.SenderSide.Selectors {
+			senderSelectors = append(senderSelectors, ir.Selector{
+				RecField: selector.RecField,
+				ArrIdx:   selector.ArrIdx,
+			})
+		}
+		result.Connections = append(result.Connections, ir.Connection{
+			SenderSide: ir.ConnectionSide{
+				PortAddr:  senderAddr,
+				Selectors: senderSelectors,
+			},
+			ReceiverSides: []ir.ConnectionSide{},
+		})
+	}
+
+	return nil
 }
