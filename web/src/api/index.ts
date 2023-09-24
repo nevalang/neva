2b053117b90e8167c6fb960aac1d6a6dbe0c1e34
Emil Valeev <emil.musician@gmail.com>
2023-09-25 01:06:45 +0600

    feat(web): remove :D

--- web/src/api/index.ts
+++
@@ -1,9 +0,0 @@
-import { Operator, Program } from "~types/program"
-
-export interface Api {
-  getPaths(): Promise<string[]>
-  getProgram(path: string): Promise<Program>
-  createProgram(path: string, program: Program): Promise<Program>
-  editProgram(path: string, program: Program): Promise<Program>
-  getOperators(): Promise<{ [key: string]: Operator }>
-}
