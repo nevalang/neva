cb0f2e8bee02c871749ad74847bb113f41e9a892
Emil Valeev <emil.musician@gmail.com>
2023-09-25 01:47:31 +0600

    feat(std): add 'add', parseNum and more

--- internal/compiler/std/std.go
+++ internal/compiler/std/std.go
@@ -6,9 +6,34 @@ import (
 )
 
 // New returns single-package std library with only runtime-function interfaces. TODO add HOCs.
-func New() src.Package {
+func New() src.Package { //nolint:funlen
 	return src.Package{
 		Entities: map[string]src.Entity{
+			"Read": {
+				Exported: true,
+				Kind:     src.ComponentEntity,
+				Component: src.Component{
+					Interface: src.Interface{
+						Params: []ts.Param{},
+						IO: src.IO{
+							In: map[string]src.Port{
+								"sig": {
+									Type: ts.Expr{
+										Inst: ts.InstExpr{Ref: "any"},
+									},
+								},
+							},
+							Out: map[string]src.Port{
+								"v": {
+									Type: ts.Expr{
+										Inst: ts.InstExpr{Ref: "str"},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
 			"Print": {
 				Exported: true,
 				Kind:     src.ComponentEntity,
@@ -82,6 +107,101 @@ func New() src.Package {
 								"v": {
 									Type: ts.Expr{
 										Inst: ts.InstExpr{Ref: "t1"},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			"Add": {
+				Exported: true,
+				Kind:     src.ComponentEntity,
+				Component: src.Component{
+					Interface: src.Interface{
+						Params: []ts.Param{
+							{
+								Name: "t1",
+								Constr: ts.Expr{
+									Lit: ts.LitExpr{
+										Union: []ts.Expr{
+											{
+												Inst: ts.InstExpr{Ref: "int"},
+											},
+											{
+												Inst: ts.InstExpr{Ref: "float"},
+											},
+											{
+												Inst: ts.InstExpr{Ref: "str"},
+											},
+										},
+									},
+								},
+							},
+						},
+						IO: src.IO{
+							In: map[string]src.Port{
+								"a": {
+									Type: ts.Expr{
+										Inst: ts.InstExpr{Ref: "t1"},
+									},
+								},
+								"b": {
+									Type: ts.Expr{
+										Inst: ts.InstExpr{Ref: "t1"},
+									},
+								},
+							},
+							Out: map[string]src.Port{
+								"v": {
+									Type: ts.Expr{
+										Inst: ts.InstExpr{Ref: "t1"},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			"ParseNum": {
+				Exported: true,
+				Kind:     src.ComponentEntity,
+				Component: src.Component{
+					Interface: src.Interface{
+						Params: []ts.Param{
+							{
+								Name: "t1",
+								Constr: ts.Expr{
+									Lit: ts.LitExpr{
+										Union: []ts.Expr{
+											{
+												Inst: ts.InstExpr{Ref: "int"},
+											},
+											{
+												Inst: ts.InstExpr{Ref: "float"},
+											},
+										},
+									},
+								},
+							},
+						},
+						IO: src.IO{
+							In: map[string]src.Port{
+								"v": {
+									Type: ts.Expr{
+										Inst: ts.InstExpr{Ref: "str"},
+									},
+								},
+							},
+							Out: map[string]src.Port{
+								"v": {
+									Type: ts.Expr{
+										Inst: ts.InstExpr{Ref: "t1"},
+									},
+								},
+								"err": {
+									Type: ts.Expr{
+										Inst: ts.InstExpr{Ref: "str"},
 									},
 								},
 							},
