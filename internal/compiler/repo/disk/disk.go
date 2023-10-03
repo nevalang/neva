ea934c51a65117147f64765824b540f0fbf88ef5
Emil Valeev <emil.musician@gmail.com>
2023-10-03 14:15:05 +0600

    wip: repo disk

--- internal/compiler/repo/disk/disk.go
+++ internal/compiler/repo/disk/disk.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/nevalang/neva/internal/compiler"
 	"github.com/nevalang/neva/pkg/ir"
@@ -15,19 +16,19 @@ type Repository struct {
 }
 
 func (r Repository) ByPath(ctx context.Context, pathToMainPkg string) (map[string]compiler.RawPackage, error) {
-	mainPkgFiles, err := readAllFilesInDir(pathToMainPkg)
-	if err != nil {
+	mainPkgFiles := map[string][]byte{}
+	if err := readAllFilesInDir(pathToMainPkg, mainPkgFiles); err != nil {
 		return nil, err
 	}
 
-	stdPkg, err := readAllFilesInDir(r.stdPath)
-	if err != nil {
+	stdFiles := map[string][]byte{}
+	if err := readAllFilesInDir(r.stdPath, stdFiles); err != nil {
 		return nil, err
 	}
 
 	return map[string]compiler.RawPackage{
 		"main": mainPkgFiles,
-		"std":  stdPkg,
+		"std":  stdFiles,
 	}, nil
 }
 
@@ -39,26 +40,32 @@ func (r Repository) Save(ctx context.Context, path string, prog *ir.Program) err
 	return os.WriteFile(path, bb, 0644) //nolint:gofumpt
 }
 
-func readAllFilesInDir(path string) (map[string][]byte, error) {
-	files := make(map[string][]byte)
-
+func readAllFilesInDir(path string, files map[string][]byte) error {
 	if err := filepath.Walk(path, func(filePath string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
-		if !info.IsDir() {
-			fileBytes, err := os.ReadFile(filePath)
-			if err != nil {
-				return err
-			}
-			files[filePath] = fileBytes
+		if info.IsDir() {
+			// err := readAllFilesInDir(filePath, files)
+			// if err != nil {
+			// 	return err
+			// }
+			return nil
 		}
+		ext := filepath.Ext(info.Name())
+		if ext != ".neva" {
+			return nil
+		}
+		bb, err := os.ReadFile(filePath)
+		if err != nil {
+			return err
+		}
+		files[strings.TrimSuffix(info.Name(), ext)] = bb
 		return nil
 	}); err != nil {
-		return nil, err
+		return err
 	}
-
-	return files, nil
+	return nil
 }
 
 func MustNew(stdPkgPath string) Repository {
