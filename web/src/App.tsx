4b192dfa18c5ead13693892ba3d09c3ed14a6248
Emil Valeev <emil.musician@gmail.com>
2023-10-13 22:40:38 +0600

    feat(web): add react-flow example

---
+++ web/src/App.tsx
@@ -0,0 +1,51 @@
+import { useCallback } from "react";
+import ReactFlow, {
+  MiniMap,
+  Controls,
+  Background,
+  useNodesState,
+  useEdgesState,
+  addEdge,
+  BackgroundVariant,
+  Connection,
+  Panel,
+  useNodesInitialized,
+} from "reactflow";
+import "reactflow/dist/style.css";
+
+const initialNodes = [
+  { id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
+  { id: "2", position: { x: 0, y: 100 }, data: { label: "2" } },
+];
+const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
+
+export default function App() {
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+  const onConnect = useCallback(
+    (params: Connection) => setEdges((eds) => addEdge(params, eds)),
+    [setEdges]
+  );
+
+  return (
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <ReactFlow
+        onInit={(instance) => instance.fitView()}
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+      >
+        <Panel position="top-left">top-left</Panel>
+        <Panel position="top-center">top-center</Panel>
+        <Panel position="top-right">top-right</Panel>
+        <Panel position="bottom-center">bottom-center</Panel>
+        <Controls />
+        <MiniMap />
+        <Background variant={BackgroundVariant.Dots} gap={12} size={1} />
+      </ReactFlow>
+    </div>
+  );
+}
