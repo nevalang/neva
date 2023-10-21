786c769bfcc723eb8a5c17507a5eefeb94a8c9e8
Emil Valeev <emil.musician@gmail.com>
2023-10-21 14:05:08 +0600

    fix(vscode): debug conf

--- web/webview/vite.config.ts
+++ web/webview/vite.config.ts
@@ -1,6 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`,
+      },
+    },
+  },
+});
