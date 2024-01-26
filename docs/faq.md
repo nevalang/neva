d108a516d4f9522761c4d14a3ee1f468bf80b1ca
Emil Valeev <emil.musician@gmail.com>
2024-01-27 02:57:24 +0600

    wip(stdlib): start designing the API

--- docs/faq.md
+++ docs/faq.md
@@ -7,3 +7,9 @@ When you need to preserve data-type on the output.
 E.g. `Void` component doesn't have outports so it doesn't matter what you passed in. That's why `Void` accepts `any` instead of `T`.
 
 On the other hand `Lock` needs to know the type of `v` on the input so the type of the `v` on the output is preserved. That's why it's `Void<T>`.
+
+## When to use `struct` and when to create different outports?
+
+When you have _structured_ data data use `struct`, when you ant to _separate dataflows_ - create outports.
+
+Example: `ParseInt` sends value and error but never at the same time. It's always one of them firing depending on the input.
--- internal/runtime/funcs/add.go
+++ internal/runtime/funcs/add.go
@@ -2,6 +2,7 @@ package funcs
 
 import (
 	"context"
+	"errors"
 
 	"github.com/nevalang/neva/internal/runtime"
 )
@@ -9,12 +10,7 @@ import (
 type addInts struct{}
 
 func (addInts) Create(io runtime.FuncIO, _ runtime.Msg) (func(ctx context.Context), error) {
-	ain, err := io.In.Port("a")
-	if err != nil {
-		return nil, err
-	}
-
-	bin, err := io.In.Port("b")
+	vvin, err := io.In.Port("vv")
 	if err != nil {
 		return nil, err
 	}
@@ -29,55 +25,108 @@ func (addInts) Create(io runtime.FuncIO, _ runtime.Msg) (func(ctx context.Contex
 			select {
 			case <-ctx.Done():
 				return
-			case v1 := <-ain:
-				select {
-				case <-ctx.Done():
-					return
-				case v2 := <-bin:
-					select {
-					case <-ctx.Done():
-						return
-					case vout <- runtime.NewIntMsg(v1.Int() + v2.Int()):
-					}
-				}
+			case subStreamItem := <-vvin:
+				subStreamItem
 			}
 		}
 	}, nil
 }
 
-type addFloats struct{}
+// type addFloats struct{}
 
-func (a addFloats) Create(io runtime.FuncIO, _ runtime.Msg) (func(ctx context.Context), error) {
-	ain, err := io.In.Port("a")
-	if err != nil {
-		return nil, err
-	}
-	bin, err := io.In.Port("b")
-	if err != nil {
-		return nil, err
-	}
-	vout, err := io.Out.Port("v")
-	if err != nil {
-		return nil, err
-	}
+// func (a addFloats) Create(io runtime.FuncIO, _ runtime.Msg) (func(ctx context.Context), error) {
+// 	vvin, ok := io.In["vv"]
+// 	if !ok {
+// 		return nil, errors.New("vv port not found")
+// 	}
 
-	return func(ctx context.Context) {
-		for {
-			select {
-			case <-ctx.Done():
-				return
-			case v1 := <-ain:
-				select {
-				case <-ctx.Done():
-					return
-				case v2 := <-bin:
-					select {
-					case <-ctx.Done():
-						return
-					case vout <- runtime.NewFloatMsg(v1.Float() + v2.Float()):
-					}
-				}
-			}
-		}
-	}, nil
-}
+// 	vout, err := io.Out.Port("v")
+// 	if err != nil {
+// 		return nil, err
+// 	}
+
+// 	return func(ctx context.Context) {
+// 		for {
+// 			var sum float64
+// 			for _, vin := range vvin {
+// 				select {
+// 				case <-ctx.Done():
+// 					return
+// 				case msg := <-vin:
+// 					sum += msg.Float()
+// 				}
+// 			}
+// 			select {
+// 			case <-ctx.Done():
+// 				return
+// 			case vout <- runtime.NewFloatMsg(sum):
+// 			}
+// 		}
+// 	}, nil
+// }
+
+// type addStrings struct{}
+
+// func (a addStrings) Create(io runtime.FuncIO, _ runtime.Msg) (func(ctx context.Context), error) {
+// 	vvin, ok := io.In["vv"]
+// 	if !ok {
+// 		return nil, errors.New("vv port not found")
+// 	}
+
+// 	vout, err := io.Out.Port("v")
+// 	if err != nil {
+// 		return nil, err
+// 	}
+
+// 	return func(ctx context.Context) {
+// 		for {
+// 			var sum string
+// 			for _, vin := range vvin {
+// 				select {
+// 				case <-ctx.Done():
+// 					return
+// 				case msg := <-vin:
+// 					sum += msg.Str()
+// 				}
+// 			}
+// 			select {
+// 			case <-ctx.Done():
+// 				return
+// 			case vout <- runtime.NewStrMsg(sum):
+// 			}
+// 		}
+// 	}, nil
+// }
+
+// type addLists struct{}
+
+// func (a addLists) Create(io runtime.FuncIO, _ runtime.Msg) (func(ctx context.Context), error) {
+// 	vvin, ok := io.In["vv"]
+// 	if !ok {
+// 		return nil, errors.New("vv port not found")
+// 	}
+
+// 	vout, err := io.Out.Port("v")
+// 	if err != nil {
+// 		return nil, err
+// 	}
+
+// 	return func(ctx context.Context) {
+// 		for {
+// 			var sum []runtime.Msg
+// 			for _, vin := range vvin {
+// 				select {
+// 				case <-ctx.Done():
+// 					return
+// 				case msg := <-vin:
+// 					sum = append(sum, msg.List()...)
+// 				}
+// 			}
+// 			select {
+// 			case <-ctx.Done():
+// 				return
+// 			case vout <- runtime.NewStrMsg(sum):
+// 			}
+// 		}
+// 	}, nil
+// }
