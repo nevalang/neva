5dfc64e0819ee131d4f2b4f192e64476bc6163dd
Emil Valeev <emil.musician@gmail.com>
2024-01-30 11:52:30 +0600

    feat(web): remove web (vscode-extension) from this repo

--- web/webview/src/components/flow/nodes/use_zoom.ts
+++
@@ -1,7 +0,0 @@
-import { useStore } from "reactflow";
-
-export function useZoom() {
-  const isZoomMiddle = useStore((s) => s.transform[2] >= 0.6);
-  const isZoomClose = useStore((s) => s.transform[2] >= 1);
-  return { isZoomMiddle, isZoomClose };
-}
