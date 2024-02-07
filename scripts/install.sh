c7347eee86db9e708abd422f3ef3ab0338cba679
Emil Valeev <emil.musician@gmail.com>
2024-02-08 01:36:21 +0600

    feat(scripts): add sudo

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,8 +7,8 @@ BIN_NAME="neva"
 echo "Downloading $BIN_NAME..."
 curl -L $BIN_URL -o $BIN_NAME
 chmod +x $BIN_NAME
-mv $BIN_NAME $INSTALL_DIR
+sudo mv $BIN_NAME $INSTALL_DIR
 echo "$BIN_NAME installed successfully to $INSTALL_DIR"
 
 # example usage:
-# curl -sSL https://raw.githubusercontent.com/nevalang/neva/main/install.sh | bash
+# curl -sSL https://raw.githubusercontent.com/nevalang/neva/main/scripts/install.sh | bash
