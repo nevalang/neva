8a993f85f0eefba8ab1833fabad65b2b9e2bac95
Emil Valeev <emil.musician@gmail.com>
2024-04-09 22:41:45 +0500

    feat(desugarer:net): support lonely port addrs

--- internal/compiler/desugarer/network.go
+++ internal/compiler/desugarer/network.go
@@ -6,16 +6,17 @@ import (
 
 	"github.com/nevalang/neva/internal/compiler"
 	src "github.com/nevalang/neva/internal/compiler/sourcecode"
+	"github.com/nevalang/neva/internal/compiler/sourcecode/core"
 )
 
 type handleNetResult struct {
 	desugaredConnections []src.Connection     // desugared network
 	virtualConstants     map[string]src.Const // constants that needs to be inserted in to make desugared network work
 	virtualNodes         map[string]src.Node  // nodes that needs to be inserted in to make desugared network work
-	usedNodePorts        nodePortsMap         // ports that were used in processed network
+	usedNodePorts        nodePortsMap         // to find unused to create virtual del connections
 }
 
-func (d Desugarer) handleNetwork( //nolint:funlen
+func (d Desugarer) handleNetwork(
 	net []src.Connection,
 	nodes map[string]src.Node,
 	scope src.Scope,
@@ -26,42 +27,54 @@ func (d Desugarer) handleNetwork( //nolint:funlen
 	constantsToInsert := map[string]src.Const{}
 
 	for _, conn := range net {
-		// array bypass connections don't need desugared,
-		// they are always portAddr->portAddr, so mark those addrs as used
-		if arrBypass := conn.ArrayBypass; arrBypass != nil {
+		if conn.ArrayBypass != nil {
 			usedNodePorts.set(
-				arrBypass.SenderOutport.Node,
-				arrBypass.SenderOutport.Port,
+				conn.ArrayBypass.SenderOutport.Node,
+				conn.ArrayBypass.SenderOutport.Port,
 			)
 			usedNodePorts.set(
-				arrBypass.ReceiverInport.Node,
-				arrBypass.ReceiverInport.Port,
+				conn.ArrayBypass.ReceiverInport.Node,
+				conn.ArrayBypass.ReceiverInport.Port,
 			)
 			desugaredConns = append(desugaredConns, conn)
 			continue
 		}
 
-		// now we only care normal connections
-
-		// if sender is port address, mark it as used
 		if conn.Normal.SenderSide.PortAddr != nil {
+			if conn.Normal.SenderSide.PortAddr.Port == "" { // if port is not specified, use first available
+				io, err := getNodeIOByPortAddr(scope, nodes, conn.Normal.SenderSide.PortAddr)
+				if err != nil {
+					return handleNetResult{}, err
+				}
+				for outport := range io.Out {
+					conn = src.Connection{
+						Normal: &src.NormalConnection{
+							SenderSide: src.ConnectionSenderSide{
+								PortAddr: &src.PortAddr{
+									Node: conn.Normal.SenderSide.PortAddr.Node,
+									Port: outport, // <- substituted
+									Idx:  conn.Normal.SenderSide.PortAddr.Idx,
+									Meta: core.Meta{},
+								},
+								Selectors: conn.Normal.SenderSide.Selectors,
+								Meta:      conn.Normal.SenderSide.Meta,
+							},
+							ReceiverSide: conn.Normal.ReceiverSide,
+						},
+						Meta: conn.Meta,
+					}
+					conn.Normal.SenderSide.PortAddr.Port = outport
+					break
+				}
+			}
+
 			usedNodePorts.set(
 				conn.Normal.SenderSide.PortAddr.Node,
 				conn.Normal.SenderSide.PortAddr.Port,
 			)
 		}
 
-		// some normal connections doesn't need to be desugared
-		if conn.Normal.SenderSide.Const == nil &&
-			len(conn.Normal.SenderSide.Selectors) == 0 &&
-			len(conn.Normal.ReceiverSide.DeferredConnections) == 0 {
-			desugaredConns = append(desugaredConns, conn)
-			continue
-		}
-
-		// === Actual desugaring starts here ===
-
-		// connections with senders with struct selectors must be desugared
+		// TODO possible option is to check whether we do not need to desugar anything here
 		if len(conn.Normal.SenderSide.Selectors) != 0 {
 			result, err := d.desugarStructSelectors(*conn.Normal)
 			if err != nil {
@@ -77,7 +90,6 @@ func (d Desugarer) handleNetwork( //nolint:funlen
 			desugaredConns = append(desugaredConns, result.connToInsert)
 		}
 
-		// connections where sender is constant also need desugarign
 		if conn.Normal.SenderSide.Const != nil { //nolint:nestif
 			if conn.Normal.SenderSide.Const.Ref != nil {
 				result, err := d.handleConstRefSender(conn, scope)
@@ -97,8 +109,23 @@ func (d Desugarer) handleNetwork( //nolint:funlen
 			}
 		}
 
-		// the last thing we could desugar is deferred connections
 		if len(conn.Normal.ReceiverSide.DeferredConnections) == 0 {
+			for i, receiver := range conn.Normal.ReceiverSide.Receivers {
+				if receiver.PortAddr.Port == "" {
+					io, err := getNodeIOByPortAddr(scope, nodes, &receiver.PortAddr)
+					if err != nil {
+						return handleNetResult{}, err
+					}
+					for inport := range io.In {
+						receiver.PortAddr.Port = inport
+						break
+					}
+					conn.Normal.ReceiverSide.Receivers[i].PortAddr.Port = receiver.PortAddr.Port // <- substituted (mutation)
+				}
+
+				usedNodePorts.set(receiver.PortAddr.Node, receiver.PortAddr.Port)
+			}
+
 			desugaredConns = append(desugaredConns, conn)
 			continue
 		}
@@ -112,12 +139,10 @@ func (d Desugarer) handleNetwork( //nolint:funlen
 			return handleNetResult{}, err
 		}
 
-		// handleThenConns recursively calls this function so it returns the same structure
 		maps.Copy(usedNodePorts.m, deferredConnsResult.usedNodesPorts.m)
 		maps.Copy(constantsToInsert, deferredConnsResult.virtualConstants)
 		maps.Copy(nodesToInsert, deferredConnsResult.virtualNodes)
 
-		// note that we discard original connection, it's desugared version is there
 		desugaredConns = append(
 			desugaredConns,
 			deferredConnsResult.desugaredConnections...,
@@ -130,4 +155,28 @@ func (d Desugarer) handleNetwork( //nolint:funlen
 		virtualConstants:     constantsToInsert,
 		virtualNodes:         nodesToInsert,
 	}, nil
+}
+
+func getNodeIOByPortAddr(
+	scope src.Scope,
+	nodes map[string]src.Node,
+	portAddr *src.PortAddr,
+) (src.IO, *compiler.Error) {
+	entity, _, err := scope.Entity(nodes[portAddr.Node].EntityRef)
+	if err != nil {
+		return src.IO{}, &compiler.Error{
+			Err:      err,
+			Location: &scope.Location,
+			Meta:     &portAddr.Meta,
+		}
+	}
+
+	var iface src.Interface
+	if entity.Kind == src.InterfaceEntity {
+		iface = entity.Interface
+	} else {
+		iface = entity.Component.Interface
+	}
+
+	return iface.IO, nil
 }
