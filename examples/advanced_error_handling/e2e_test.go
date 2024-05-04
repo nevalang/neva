4831a3075812b09fd0a7ce2997f88d9f38b22beb
Emil Valeev <emil.musician@gmail.com>
2024-05-04 20:38:40 +0500

    fix(example, e2e): advanced error handling

--- examples/advanced_error_handling/e2e_test.go
+++ examples/advanced_error_handling/e2e_test.go
@@ -9,20 +9,21 @@ import (
 )
 
 func Test(t *testing.T) {
-	err := os.Chdir("../..")
+	err := os.Chdir("..")
 	require.NoError(t, err)
 
 	wd, err := os.Getwd()
 	require.NoError(t, err)
 	defer os.Chdir(wd)
 
-	cmd := exec.Command("neva", "run", "http_get")
+	cmd := exec.Command("neva", "run", "advanced_error_handling")
 	out, err := cmd.CombinedOutput()
 	require.NoError(t, err)
-	require.Contains(
+	require.Equal(
 		t,
+		`panic: {"text": "Get \"definitely%20not%20a%20valid%20URL\":  unsupported protocol scheme \"\""}
+`,
 		string(out),
-		"panic: ",
 	)
 
 	require.Equal(t, 0, cmd.ProcessState.ExitCode())
