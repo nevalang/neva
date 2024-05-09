7c5f3e008be38aecd590b9271212de9f96693be7
dorian3343 <dorian3343@gmail.com>
2024-05-09 12:50:10 +0200

    if implementation

---
+++ internal/runtime/funcs/if.go
@@ -0,0 +1,51 @@
+package funcs
+
+import (
+	"context"
+	"github.com/nevalang/neva/internal/runtime"
+)
+
+type if_ struct{}
+
+func (p if_) Create(io runtime.FuncIO, _ runtime.Msg) (func(ctx context.Context), error) {
+	valIn, err := io.In.Port("val")
+	if err != nil {
+		return nil, err
+	}
+
+	okOut, err := io.Out.Port("ok")
+	if err != nil {
+		return nil, err
+	}
+
+	elseOut, err := io.Out.Port("else")
+	if err != nil {
+		return nil, err
+	}
+
+	return func(ctx context.Context) {
+		var (
+			val1 runtime.Msg
+		)
+
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case val1 = <-valIn:
+			}
+
+			select {
+			case <-ctx.Done():
+				return
+
+			default:
+				if val1.Bool() == true {
+					okOut <- runtime.NewBoolMsg(true)
+				} else {
+					elseOut <- runtime.NewBoolMsg(false)
+				}
+			}
+		}
+	}, nil
+}
