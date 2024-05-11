7a8a26a99f931289212f933e88baef3cf0f5dafd
Emil Valeev <emil.musician@gmail.com>
2024-05-11 19:36:00 +0500

    feat(e2e): add e2e test for "neva new" cli cmd

---
+++ e2e/cli_new/e2e_test.go
@@ -0,0 +1,30 @@
+package test
+
+import (
+	"os"
+	"os/exec"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func Test(t *testing.T) {
+	defer func() {
+		require.NoError(t, os.RemoveAll("src"))
+	}()
+
+	cmd := exec.Command("neva", "new")
+	require.NoError(t, cmd.Run())
+
+	cmd = exec.Command("neva", "run", "src")
+
+	out, err := cmd.CombinedOutput()
+	require.NoError(t, err)
+	require.Equal(
+		t,
+		"Hello, World!\n",
+		string(out),
+	)
+
+	require.Equal(t, 0, cmd.ProcessState.ExitCode())
+}
