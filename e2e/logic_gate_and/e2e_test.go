892d3a228e4d706d38321cac6b6dea22e3f79646
dorian3343 <dorian3343@gmail.com>
2024-05-12 23:19:16 +0200

    fixed example

---
+++ e2e/logic_gate_and/e2e_test.go
@@ -0,0 +1,23 @@
+package test
+
+import (
+	"os/exec"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+// Check that math example with multiplying numbers by using port bridge works as expected.
+func TestMathMultiplyNumbers(t *testing.T) {
+	cmd := exec.Command("neva", "run", "main")
+
+	out, err := cmd.CombinedOutput()
+	require.NoError(t, err)
+	require.Equal(
+		t,
+		"true\n",
+		string(out),
+	)
+
+	require.Equal(t, 0, cmd.ProcessState.ExitCode())
+}
