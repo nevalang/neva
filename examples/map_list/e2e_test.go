ff0982caaed1668da11d0e92f3aedd8f06e3248b
Emil Valeev <emil.musician@gmail.com>
2024-05-12 22:58:19 +0500

    feat(std): lists.Map

--- examples/map_list/e2e_test.go
+++ examples/map_list/e2e_test.go
@@ -16,19 +16,16 @@ func Test(t *testing.T) {
 	require.NoError(t, err)
 	defer os.Chdir(wd)
 
-	for i := 0; i < 100; i++ {
-		cmd := exec.Command("neva", "run", "map_list")
+	cmd := exec.Command("neva", "run", "map_list")
 
-		out, err := cmd.CombinedOutput()
-		require.NoError(t, err)
+	out, err := cmd.CombinedOutput()
+	require.NoError(t, err)
 
-		require.Equal(
-			t,
-			"[49,29,19,99]\n",
-			string(out),
-			"iteration %d", i,
-		)
+	require.Equal(
+		t,
+		"[49,29,19,99]\n",
+		string(out),
+	)
 
-		require.Equal(t, 0, cmd.ProcessState.ExitCode())
-	}
+	require.Equal(t, 0, cmd.ProcessState.ExitCode())
 }
