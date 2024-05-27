ecf3841f351127b24cbdf988fb6c5317d282c9f5
Emil Valeev <emil.musician@gmail.com>
2024-05-28 02:44:57 +0500

    Update version.go

--- pkg/version.go
+++ pkg/version.go
@@ -2,4 +2,4 @@ package pkg
 
 // Version is the current version of the language and stdlib.
 // Don't forget to update it before release new tag.
-var Version = "0.24.0" //nolint:gochecknoglobals
+var Version = "0.25.0" //nolint:gochecknoglobals
