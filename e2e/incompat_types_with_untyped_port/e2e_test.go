262078ca6a6929b5a6bf595f926c2ff72eeb08c7
Emil Valeev <emil.musician@gmail.com>
2024-06-08 17:01:10 +0500

    fix(compiler:typesystem): improve errors for connections without specifying ports

---
+++ e2e/incompat_types_with_untyped_port/e2e_test.go
@@ -0,0 +1,23 @@
+package test
+
+import (
+	"os/exec"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func Test(t *testing.T) {
+	cmd := exec.Command("neva", "run", "main")
+
+	out, err := cmd.CombinedOutput()
+	require.NoError(t, err)
+
+	require.Equal(
+		t,
+		"main/main.neva:19:8 Incompatible types: in:in -> add: Subtype and supertype must both be either literals or instances, except if supertype is union: expression any, constaint { data int, idx int, last bool }\n",
+		string(out),
+	)
+
+	require.Equal(t, 0, cmd.ProcessState.ExitCode())
+}
