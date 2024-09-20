a3f042ab69f2cc5978adc7dba0ba2a9aa37b95d2
Emil Valeev <emil.musician@gmail.com>
2024-09-20 14:13:14 +0500

    fix(tests): improve tests output

--- e2e/incompat_types_with_untyped_port/e2e_test.go
+++ e2e/incompat_types_with_untyped_port/e2e_test.go
@@ -2,6 +2,7 @@ package test
 
 import (
 	"os/exec"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -13,11 +14,27 @@ func Test(t *testing.T) {
 	out, err := cmd.CombinedOutput()
 	require.NoError(t, err)
 
-	require.Equal(
+	// TODO simplify when struct fields will have order: https://github.com/nevalang/neva/issues/698
+
+	expectedErrorPrefix := "main/main.neva:16:8 Incompatible types: in:in -> add: Subtype and supertype must both be either literals or instances, except if supertype is union: expression any, constraint {"
+	expectedErrorSuffix := "}\n"
+	expectedFields := []string{"data int", "idx int", "last bool"}
+
+	actualOutput := string(out)
+	require.True(
 		t,
-		"main/main.neva:16:8 Incompatible types: in:in -> add: Subtype and supertype must both be either literals or instances, except if supertype is union: expression any, constraint { data int, idx int, last bool }\n",
-		string(out),
+		strings.HasPrefix(actualOutput, expectedErrorPrefix),
+		"Error message should start with expected prefix",
 	)
+	require.True(
+		t,
+		strings.HasSuffix(actualOutput, expectedErrorSuffix),
+		"Error message should end with expected suffix")
+
+	// Check if all expected fields are present in the error message
+	for _, field := range expectedFields {
+		require.Contains(t, actualOutput, field, "Error message should contain field: "+field)
+	}
 
 	require.Equal(t, 0, cmd.ProcessState.ExitCode())
 }
