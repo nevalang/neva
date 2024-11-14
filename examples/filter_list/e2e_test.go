b6fabdf48b314af55bbffac348a8df2adf19b9c9
Emil Valeev <emil.musician@gmail.com>
2024-11-15 01:33:54 +0500

    fix(e2e,examples): everything fixed except filter list example

--- examples/filter_list/e2e_test.go
+++ examples/filter_list/e2e_test.go
@@ -19,7 +19,7 @@ func Test(t *testing.T) {
 	defer os.Chdir(wd)
 
 	for i := 0; i < 1; i++ {
-		cmd := exec.Command("neva", "run", "filter_list")
+		cmd := exec.Command("neva", "run", "-trace", "filter_list")
 
 		// Set a timeout for the command
 		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
