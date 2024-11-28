4147ad8f99cfe09d96f6b51dd0c2898b0ee5460b
Emil Valeev <emil.musician@gmail.com>
2024-11-28 22:02:35 +0500

    feat(version): bump to 0.28

--- pkg/version.go
+++ pkg/version.go
@@ -2,4 +2,4 @@ package pkg
 
 // Version is the current version of the language and stdlib.
 // Don't forget to update it before release new tag.
-var Version = "0.27.1" //nolint:gochecknoglobals
+var Version = "0.28.0" //nolint:gochecknoglobals
