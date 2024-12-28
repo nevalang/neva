6e6ce2deee9fedafa949c770045d528b8ec8a67f
Emil Valeev <emil.musician@gmail.com>
2024-12-28 21:36:34 +0500

    feat(e2e,version): bump to 29.1 AND add e2e for the fix from prev commit

--- pkg/version.go
+++ pkg/version.go
@@ -2,4 +2,4 @@ package pkg
 
 // Version is the current version of the language and stdlib.
 // Don't forget to update it before release new tag.
-var Version = "0.29.0" //nolint:gochecknoglobals
+var Version = "0.29.1" //nolint:gochecknoglobals
