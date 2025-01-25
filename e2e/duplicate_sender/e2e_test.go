18206f6fed10caf3e583fc56a6b75e3f64955dbb
Emil Valeev <emil.musician@gmail.com>
2025-01-26 00:11:47 +0500

    fix(e2e): update after compiler send exitcode=1 when compilation fails

--- e2e/duplicate_sender/e2e_test.go
+++ e2e/duplicate_sender/e2e_test.go
@@ -10,16 +10,12 @@ import (
 
 func Test(t *testing.T) {
 	cmd := exec.Command("neva", "run", "main")
-
 	cmd.Stdin = strings.NewReader("yo\n")
-	out, err := cmd.CombinedOutput()
-	require.NoError(t, err)
-
+	out, _ := cmd.CombinedOutput()
+	require.Equal(t, 1, cmd.ProcessState.ExitCode())
 	require.Contains(
 		t,
 		string(out),
 		"main/main.neva:7:1: port 'in:start' is used twice\n",
 	)
-
-	require.Equal(t, 0, cmd.ProcessState.ExitCode())
 }
