a83342a52f8b28c16a96b09ff397fbabace64b80
Emil Valeev <emil.musician@gmail.com>
2025-02-09 22:20:42 +0500

    feat(std:io, examples, e2e): rename write_all:sig -> :res

--- internal/runtime/funcs/write_all.go
+++ internal/runtime/funcs/write_all.go
@@ -10,47 +10,47 @@ import (
 type writeAll struct{}
 
 func (c writeAll) Create(rio runtime.IO, _ runtime.Msg) (func(ctx context.Context), error) {
-	filename, err := rio.In.Single("filename")
+	filenameIn, err := rio.In.Single("filename")
 	if err != nil {
 		return nil, err
 	}
 
-	dataPort, err := rio.In.Single("data")
+	dataIn, err := rio.In.Single("data")
 	if err != nil {
 		return nil, err
 	}
 
-	sig, err := rio.Out.Single("sig")
+	resOut, err := rio.Out.Single("res")
 	if err != nil {
 		return nil, err
 	}
 
-	errPort, err := rio.Out.Single("err")
+	errOut, err := rio.Out.Single("err")
 	if err != nil {
 		return nil, err
 	}
 
 	return func(ctx context.Context) {
 		for {
-			name, ok := filename.Receive(ctx)
+			filenameMsg, ok := filenameIn.Receive(ctx)
 			if !ok {
 				return
 			}
 
-			data, ok := dataPort.Receive(ctx)
+			dataMsg, ok := dataIn.Receive(ctx)
 			if !ok {
 				return
 			}
 
-			err := os.WriteFile(name.Str(), []byte(data.Str()), 0755)
+			err := os.WriteFile(filenameMsg.Str(), []byte(dataMsg.Str()), 0755)
 			if err != nil {
-				if !errPort.Send(ctx, errFromErr(err)) {
+				if !errOut.Send(ctx, errFromErr(err)) {
 					return
 				}
 				continue
 			}
 
-			if !sig.Send(ctx, emptyStruct()) {
+			if !resOut.Send(ctx, emptyStruct()) {
 				return
 			}
 		}
