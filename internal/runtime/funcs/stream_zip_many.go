cf3d7b95d34518b3fc7caa278ceaa520b1ce93f9
Emil Valeev <emil.musician@gmail.com>
2025-10-25 20:42:30 +0700

    Fix stream zip many to collect full streams before zipping

--- internal/runtime/funcs/stream_zip_many.go
+++ internal/runtime/funcs/stream_zip_many.go
@@ -2,6 +2,8 @@ package funcs
 
 import (
 	"context"
+	"sync"
+	"sync/atomic"
 
 	"github.com/nevalang/neva/internal/runtime"
 )
@@ -24,42 +26,83 @@ func (streamZipMany) Create(
 
 	return func(ctx context.Context) {
 		streamsCount := dataIn.Len()
-		index := int64(0)
+		if streamsCount == 0 {
+			return
+		}
 
 		for {
-			zipped := make([]runtime.Msg, streamsCount)
-			shouldStop := false
+			type streamState struct {
+				data []runtime.Msg
+			}
+
+			states := make([]streamState, streamsCount)
+			var shouldStop atomic.Bool
+			var aborted atomic.Bool
+
+			var wg sync.WaitGroup
+			wg.Add(streamsCount)
 
 			for streamIdx := 0; streamIdx < streamsCount; streamIdx++ {
-				msg, ok := dataIn.Receive(ctx, streamIdx)
-				if !ok {
+				idx := streamIdx
+
+				go func() {
+					defer wg.Done()
+
+					collected := make([]runtime.Msg, 0)
+
+					for {
+						msg, ok := dataIn.Receive(ctx, idx)
+						if !ok {
+							aborted.Store(true)
+							return
+						}
+
+						item := msg.Struct()
+						collected = append(collected, item.Get("data"))
+
+						if item.Get("last").Bool() {
+							shouldStop.Store(true)
+							states[idx] = streamState{data: collected}
+							return
+						}
+					}
+				}()
+			}
+
+			wg.Wait()
+
+			if aborted.Load() {
+				return
+			}
+
+			count := len(states[0].data)
+			for streamIdx := 1; streamIdx < streamsCount; streamIdx++ {
+				if l := len(states[streamIdx].data); l < count {
+					count = l
+				}
+			}
+
+			for idx := 0; idx < count; idx++ {
+				zipped := make([]runtime.Msg, streamsCount)
+				for streamIdx := 0; streamIdx < streamsCount; streamIdx++ {
+					zipped[streamIdx] = states[streamIdx].data[idx]
+				}
+
+				if !resOut.Send(
+					ctx,
+					streamItem(
+						runtime.NewListMsg(zipped),
+						int64(idx),
+						idx == count-1,
+					),
+				) {
 					return
 				}
-
-				item := msg.Struct()
-				zipped[streamIdx] = item.Get("data")
-
-				if item.Get("last").Bool() {
-					shouldStop = true
-				}
 			}
 
-			if !resOut.Send(
-				ctx,
-				streamItem(
-					runtime.NewListMsg(zipped),
-					index,
-					shouldStop,
-				),
-			) {
+			if shouldStop.Load() || count == 0 {
 				return
 			}
-
-			if shouldStop {
-				return
-			}
-
-			index++
 		}
 	}, nil
 }
