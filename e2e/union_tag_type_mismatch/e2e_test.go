82d0c582d4c4b8b85fabcda561f221335d185b55
Emil Valeev <emil.musician@gmail.com>
2026-01-23 21:57:05 +0700

    feat(e2e:union): add more tests

---
+++ e2e/union_tag_type_mismatch/e2e_test.go
@@ -0,0 +1,14 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/nevalang/neva/pkg/e2e"
+	"github.com/stretchr/testify/require"
+)
+
+func Test(t *testing.T) {
+	_, stderr := e2e.Run(t, []string{"run", "main"}, e2e.WithCode(1))
+	require.Contains(t, stderr, "Union tag type")
+	require.Contains(t, stderr, "does not match Union<T> type")
+}
